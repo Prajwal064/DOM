ec6119c989e72dabec0901fab1420779dfdf8b70
RAMITA LAPTOP <you@example.com>
2026-03-16 23:26:31 +0545

    first commit

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-let btn = document.querySelectorAll("button")
+// let btn = document.querySelectorAll("button")
 
 
-btn.forEach(function(ele){
-    ele.addEventListener("click", function(){
-        console.log("Hello");
-        if(ele.innerHTML === 'Add Friend'){
-            ele.innerHTML === 'Remove'
-        }
-        else{
-            ele.innerHTML == "Add Friend"
-        }
+// btn.forEach(function(ele){
+//     ele.addEventListener("click", function(){
+//         console.log("Hello");
+//         if(ele.innerHTML === 'Add Friend'){
+//             ele.innerHTML === 'Remove'
+//         }
+//         else{
+//             ele.innerHTML == "Add Friend"
+//         }
         
-    })
-})
+//     })
+// })
+
+
+class Book{
+    constructor (name, price, author){
+        this.name = name;
+        this.price = price;
+        this.author = author;
+    }
+}
+
+
+let b1 = new Book("Python", "600", "Milan")
+let b2 = new Book("JS", "800", "Prajwal")
+let b3 = new Book("C++", "100", "Dhole")
+
+
+
+
+
